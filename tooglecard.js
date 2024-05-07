b6d0312613cd1daf8cdbbab100803f7a68dd1682
Maurício Moura da Veiga <mauricioveiga27@gmail.com>
2024-05-07 20:01:07 -0300

    .

---
+++ tooglecard.js
@@ -0,0 +1,13 @@
+function toggleCard(card) {
+    const cardBody = card.querySelector('.card__body');
+    const partialText = cardBody.querySelector('.partial-text');
+    const fullText = cardBody.querySelector('.full-text');
+    
+    if (partialText.style.display === 'none') {
+      partialText.style.display = 'block';
+      fullText.style.display = 'none';
+    } else {
+      partialText.style.display = 'none';
+      fullText.style.display = 'block';
+    }
+  }
